fb31ed83f491ae3fbbb8f90e3f7662ec313a4572
Nataliia Zabiiaka <zabijaka.nata93@gmail.com>
2022-10-24 22:26:07 +0300

    Added some changes

--- lib/screens/destination_screen.dart
+++ lib/screens/destination_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import '../models/destination_model.dart';
+import '../models/activity_model.dart';
 
 class DestinationScreen extends StatefulWidget {
 
   final Destination destination;
 
-  const DestinationScreen({Key? key, this.destination}) : super(key: key);
+  const DestinationScreen({Key? key, required this.destination}) : super(key: key);
 
   @override
   _DestinationScreenState createState() => _DestinationScreenState();
@@ -13,6 +15,140 @@ class DestinationScreen extends StatefulWidget {
 class _DestinationScreenState extends State<DestinationScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black26,
+                      offset: Offset(0.0, 2.0),
+                      blurRadius: 6,
+                    ),
+                  ],
+                ),
+                child: Hero(
+                  tag: widget.destination.imageUrl,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(30),
+                    child: Image(
+                      image: AssetImage(
+                          widget.destination.imageUrl,
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 10,
+                    vertical: 40),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                        onPressed: () => Navigator.pop(context),
+                        icon: const Icon(Icons.arrow_back),
+                    iconSize: 30,
+                        color: Colors.black,
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          onPressed: () => Navigator.pop(context),
+                          icon: const Icon(Icons.search),
+                          iconSize: 30,
+                          color: Colors.black,
+                        ),
+                        IconButton(
+                          onPressed: () => Navigator.pop(context),
+                          icon: Icon(FontAwesomeIcons.sortAmountDown),
+                          iconSize: 25,
+                          color: Colors.black,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Positioned(
+                left: 20,
+                bottom: 20,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.destination.city,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 35,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 1.2,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          FontAwesomeIcons.locationArrow,
+                          color: Colors.white70,
+                          size: 15,
+                        ),
+                        const SizedBox(width: 5),
+                        Text(
+                          widget.destination.country,
+                          style: const TextStyle(
+                            color: Colors.white70,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              const Positioned(
+                  right: 20,
+                  bottom: 20,
+                  child: Icon(
+                      Icons.location_on,
+                    color: Colors.white70,
+                      size: 25,
+                  ),
+              )
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: widget.destination.activities.length,
+                itemBuilder: (BuildContext context, int index) {
+                Activity activity = widget.destination.activities[index];
+                return Stack(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(40, 5, 20, 5),
+                      height: 170,
+                        width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    )
+                  ],
+                );
+                  Text(
+                  widget.destination.activities[index].name,
+                );
+                },
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
